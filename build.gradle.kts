418108ed11d30d3ae5220dd01e05fb32d49b2fc7
Ali Sabzevari <alisabzevari@gmail.com>
2021-03-17 22:34:11 +0100

    Fix test jvm target

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,3 +67,6 @@ tasks.jacocoTestReport {
 
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions.jvmTarget = "1.8"
+
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions.jvmTarget = "1.8"
